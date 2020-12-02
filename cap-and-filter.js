89532ba3c66464a0f7fd73368fc468f87b680e18
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-12-01 22:11:08 -0800

    passing with loop

--- cap-and-filter.js
+++ cap-and-filter.js
@@ -1,5 +1,15 @@
 const capitalizeAndFilter = array => {
+    const newArray = [];
 
+    for(let i = 0; i < array.length; i++) {
+        const element = array[i];
+        const capElement = element.toUpperCase();
+
+        if (!capElement.startsWith('F')) {
+            newArray.push(capElement);
+        }
+    }
+    return newArray
 };
 
 module.exports = {
