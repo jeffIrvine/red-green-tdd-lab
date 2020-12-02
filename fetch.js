3294ddda135b4f7436882bf557fa19c0338c2b6f
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-12-01 23:08:35 -0800

    close to green

--- fetch.js
+++ fetch.js
@@ -1,6 +1,17 @@
-const fetchQuotes = array => {
+const request = require ('superagent');
 
-};
+
+const fetchQuotes = async() => {
+    const response = await request.get('futuramaapi.herokuapp.com/api/quotes')
+
+    const results = response.body[0];
+    const firstQuote = {
+        name: results.character,
+        text: results.quote,
+        image: results.image,
+
+    }
+}
 
 module.exports = {
     fetchQuotes
--- fetch.test.js
+++ fetch.test.js
@@ -1,8 +1,19 @@
 const { fetchQuotes } = require ('./fetch')
+const request = require ('superagent');
 
-describe('return a single quote from the futurama api', () => {
-    it('', () => {
-    const result = fetchQuotes()
-
-    })
+describe('return a single quote from the futurama api',  () => {
+    it('fetches one quote', async () => {
+        const quote = {
+            character: "Bender",
+            quote: "I'm a fraud. A poor, lazy, sexy fraud.",
+            image: "https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png"
+          }
+          const results = fetchQuotes(quote)
+  
+      expect(results).toEqual({ 
+          name: "Bender",
+          text: "I'm a fraud. A poor, lazy, sexy fraud.",
+          image: "https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png"
+      });
+        })
 });
