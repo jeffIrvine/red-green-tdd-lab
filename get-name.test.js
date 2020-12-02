c3e67738ad7dc30d3e9f9b7f49fa20bc18b55693
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-12-01 20:45:06 -0800

    get name step 2

--- get-name.test.js
+++ get-name.test.js
@@ -4,5 +4,6 @@ describe('getName', () => {
     it('returns an object\'s name property', () => {
         const spot = { name: 'spot', age: 5, weight: '20 lbs' }
         const name = getName(spot)
+        expect(name).toEqual('spot')
     })
 });
